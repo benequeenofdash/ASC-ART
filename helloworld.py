955ff1b187c727e1396420ef528ecb9c4c98c71c
benequeenofdash <johnanthonysanchez824@gmail.com>
2024-08-29 18:46:16 +0800

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+greeting ='           _   _ _______ _    _  ____  _   ___     __'
+greeting1='     /\   | \ | |__   __| |  | |/ __ \| \ | \ \   / /'
+greeting2='    /  \  |  \| |  | |  | |__| | |  | |  \| |\ \_/ / '
+greeting3='   / /\ \ | . ` |  | |  |  __  | |  | | . ` | \   /  '
+greeting4='  / ____ \| |\  |  | |  | |  | | |__| | |\  |  | |   '
+greeting5=' /_/    \_\_| \_|  |_|  |_|  |_|\____/|_| \_|  |_|   '
+
+print (greeting)
+print (greeting1)
+print (greeting2)
+print (greeting3)
+print (greeting4)
+print (greeting5)
+
+
